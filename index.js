777e5824adf2548b96ac49ceea713cba2e2bab63
Gauthaman24 <mgauthaman24@gmail.com>
2024-08-21 10:23:34 +0530

    code upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('show');
+    });
+});
